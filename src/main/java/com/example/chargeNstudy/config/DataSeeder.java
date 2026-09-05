ec8526793bebb27225923467dda7ed38564fed43
Starishine <sania.amarnath@gmail.com>
2026-09-05 15:53:40 +0800

    Fix images

--- src/main/java/com/example/chargeNstudy/config/DataSeeder.java
+++ src/main/java/com/example/chargeNstudy/config/DataSeeder.java
@@ -49,6 +49,7 @@ public class DataSeeder implements CommandLineRunner {
         // FASS
         Building as1 = getOrCreateBuilding(fass, "AS1", 1.2965182, 103.7682741);
         Building as8 = getOrCreateBuilding(fass, "AS8", 1.2965182, 103.7682741);
+        // Libraries
         Building centralLibrary = getOrCreateLibrary("Central Library", 1.2965182, 103.7682741);
         Building medSciLibrary = getOrCreateLibrary("Medicine+Science Library", 1.2969518, 103.7813775);
         Building cjKohLibrary = getOrCreateLibrary("C J Koh Law Library", 1.3071219, 103.7699999);
@@ -74,7 +75,7 @@ public class DataSeeder implements CommandLineRunner {
                 new StudySpot(null, "Wooden Benches around COM2", com2, "Open wooden benches, nature-friendly environment, very few sockets - some on the wall, great for group discussions & casual study, near CoolSpot and The Terrace",
                         StudySpot.Quantity.FEW, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.LIMITED, true, false, "24 hours", true, "study-spot-images/SoC/com2_wooden_benches.png"),
                 new StudySpot(null, "The Coffee Bean and Tea Leaf", com3, "Popular cafe with cozy atmosphere, moderate noise, seats with power sockets fill up fast, good for casual study with food",
-                        StudySpot.Quantity.MODERATE, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.MODERATE, false, true, "9am - 6pm", true, "study-spot-images/SoC/com2_coffee_bean.png"),
+                        StudySpot.Quantity.MODERATE, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.MODERATE, false, true, "9am - 6pm", true, "study-spot-images/SoC/coffee-bean.jpg"),
                 new StudySpot(null, "COM1 Level 1", com1, "Airconditioned Small area to study in between classes, Sockets available",
                         StudySpot.Quantity.FEW, StudySpot.NoiseLevel.QUIET, StudySpot.SeatingCapacity.LIMITED, true, true, "8am - 6pm", true, "study-spot-images/SoC/com1_level1.jpg"),
                 // -- FASS --
@@ -86,9 +87,9 @@ public class DataSeeder implements CommandLineRunner {
                         StudySpot.Quantity.FEW, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.LIMITED, true, false, "24 hours", true, "study-spot-images/FASS/fass_wooden_benches.jpg"),
                 // School of Business
                 new StudySpot(null, "Wooden Benches around Biz2", biz2, "Open wooden benches, nature-friendly environment, very few sockets - some on the wall, great for group discussions & casual study, near The Deck",
-                        StudySpot.Quantity.FEW, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.LIMITED, true, false, "24 hours", true, "study-spot-images/FASS/fass_wooden_benches.jpg"),
+                        StudySpot.Quantity.FEW, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.LIMITED, true, false, "24 hours", true, "study-spot-images/Biz/biz2_wooden_tables.png"),
                 new StudySpot(null, "The Spread", biz2, "Popular cafe with cozy atmosphere, very lively during peak hours, very few sockets available, good for casual study with food",
-                        StudySpot.Quantity.FEW, StudySpot.NoiseLevel.LOUD, StudySpot.SeatingCapacity.MODERATE, true, true, "8am - 6.30pm", true, "study-spot-images/FASS/biz2_spread.jpg"),
+                        StudySpot.Quantity.FEW, StudySpot.NoiseLevel.LOUD, StudySpot.SeatingCapacity.MODERATE, true, true, "8am - 6.30pm", true, "study-spot-images/Biz/biz2_spread.jpg"),
                 // -- Libraries --
                 new StudySpot(null, "Central Library L6 Study Area Zones", centralLibrary, "Multiple individual study cubicles and desks, many sockets, nearby food options - The Deck, Anna Cafe",
                         StudySpot.Quantity.MANY, StudySpot.NoiseLevel.QUIET, StudySpot.SeatingCapacity.PLENTIFUL, false, true, "9am - 9pm", true, "study-spot-images/Libraries/clb_L6.jpg"),
@@ -112,11 +113,11 @@ public class DataSeeder implements CommandLineRunner {
                 new StudySpot(null, "NUS UTown Plaza Level 2", utownPlaza, "Study area with moderate noise, limited seating, and a few power outlets. Ideal for group discussions and casual study in between classes.",
                         StudySpot.Quantity.FEW, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.LIMITED, true, false, "24 hours", true, "study-spot-images/UTown/utown-plaza-level2.jpg"),
                 new StudySpot(null, "Outside Starbucks", erc, "Open study area with power outlets, ample seating. Ideal for group discussions and night study sessions.",
-                        StudySpot.Quantity.MANY, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.PLENTIFUL, true, false, "24 hours", true, "study-spot-images/UTown/outside-starbucks.jpg"),
+                        StudySpot.Quantity.MANY, StudySpot.NoiseLevel.MODERATE, StudySpot.SeatingCapacity.PLENTIFUL, true, false, "24 hours", true, "study-spot-images/UTown/outside-starbucks.png"),
                 new StudySpot(null, "Inside Starbucks", erc, "Airconditioned quietstudy area with power outlets, cafe seating. Ideal for individual study and casual group discussions.",
                         StudySpot.Quantity.MODERATE, StudySpot.NoiseLevel.QUIET, StudySpot.SeatingCapacity.MODERATE, false, true, "24 hours", true, "study-spot-images/UTown/starbucks.jpg"),
                 new StudySpot(null, "ERC Mac & PC Commons", erc, "Quiet study area with power outlets + desktop access, ample seating. Ideal for individual study and focused work.",
-                        StudySpot.Quantity.MANY, StudySpot.NoiseLevel.QUIET, StudySpot.SeatingCapacity.PLENTIFUL, false, true, "10am - 8pm", true, "study-spot-images/UTown/erc-mac-pc-commons.jpg"),
+                        StudySpot.Quantity.MANY, StudySpot.NoiseLevel.QUIET, StudySpot.SeatingCapacity.PLENTIFUL, false, true, "10am - 8pm", true, "study-spot-images/UTown/erc-mac-pc-comms.jpg"),
                 new StudySpot(null, "The Study @ ERC Level 2", erc, "Individual study pods with power outlets, ample seating. Ideal for individual study and focused work.",
                         StudySpot.Quantity.MANY, StudySpot.NoiseLevel.QUIET, StudySpot.SeatingCapacity.PLENTIFUL, false, true, "8am - 11pm", true, "study-spot-images/UTown/erc-level2.jpg"),
                 new StudySpot(null, "Ian and Peony Ferguson Study Level 3", erc, "Quiet study area with power outlets, ample seating. Ideal for individual study and focused work.",
